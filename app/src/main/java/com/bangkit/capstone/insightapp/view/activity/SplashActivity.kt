17e9a47de1eb21b37eb27c00f3d2c949af93f079
arya-rgb <dickyarya.personal@gmail.com>
2021-05-24 08:31:13 +0700

    add splash activity and userdetail

--- app/src/main/java/com/bangkit/capstone/insightapp/view/activity/SplashActivity.kt
+++ app/src/main/java/com/bangkit/capstone/insightapp/view/activity/SplashActivity.kt
@@ -1,14 +1,38 @@
 package com.bangkit.capstone.insightapp.view.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.bangkit.capstone.insightapp.MainActivity
 import com.bangkit.capstone.insightapp.R
+import com.bangkit.capstone.insightapp.databinding.ActivitySplashBinding
+import com.bumptech.glide.Glide
 
 class SplashActivity : AppCompatActivity() {
 
+    private var _binding: ActivitySplashBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+        _binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        Glide.with(this)
+            .load(R.drawable.splash_logo)
+            .into(binding.SplashView)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+            overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+            finish()
+        }, 3000)
 
     }
 }
