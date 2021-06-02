770ab81f80cee10ee22a5d994bea6c9e6ccf3add
arya-rgb <dickyarya.personal@gmail.com>
2021-06-03 01:29:43 +0700

    add user searchview

--- app/src/main/java/com/bangkit/capstone/insightapp/view/activity/ChatActivity.kt
+++ app/src/main/java/com/bangkit/capstone/insightapp/view/activity/ChatActivity.kt
@@ -1,8 +1,12 @@
 package com.bangkit.capstone.insightapp.view.activity
 
+import android.app.SearchManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.View
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatViewInflater
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -11,7 +15,10 @@ import com.bangkit.capstone.insightapp.databinding.ActivityChatBinding
 import com.bangkit.capstone.insightapp.databinding.ActivityLoginBinding
 import com.bangkit.capstone.insightapp.model.UserModel
 import com.bangkit.capstone.insightapp.viewmodel.UserListAdapter
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class ChatActivity : AppCompatActivity() {
 
@@ -50,6 +57,58 @@ class ChatActivity : AppCompatActivity() {
                     }
                     binding.progressBarProfil.visibility = View.GONE
                     userRecyclerView.adapter = UserListAdapter(userArrayList)
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+                binding.progressBarProfil.visibility = View.GONE
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_detail_user, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search)
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                searchForUser(query.toLowerCase(Locale.ROOT))
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+
+        })
+
+        return true
+    }
+
+    private fun searchForUser(str: String) {
+
+        var fireBaseUserID = FirebaseAuth.getInstance().currentUser?.uid
+        val queryUser = FirebaseDatabase.getInstance().reference
+            .child("Users").orderByChild("search")
+            .startAt(str)
+            .endAt(str + "\uf8ff")
+
+        queryUser.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    for (userSnapshot in snapshot.children) {
+                        val user = userSnapshot.getValue(UserModel::class.java)
+                        userArrayList.clear()
+                        userArrayList.add(user!!)
+                        binding.progressBarProfil.visibility = View.GONE
+                    }
+                    binding.progressBarProfil.visibility = View.GONE
+                    userRecyclerView.adapter = UserListAdapter(userArrayList)
+
                 }
             }
             override fun onCancelled(error: DatabaseError) {
