e2b797af616404695f62cd7e372943ad46783fb5
arya-rgb <dickyarya.personal@gmail.com>
2021-06-03 10:33:33 +0700

    fix tensorflow code and add edit text validation

--- app/src/main/java/com/bangkit/capstone/insightapp/view/activity/FindOutActivity.kt
+++ app/src/main/java/com/bangkit/capstone/insightapp/view/activity/FindOutActivity.kt
@@ -3,6 +3,9 @@ package com.bangkit.capstone.insightapp.view.activity
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.core.content.ContextCompat
 import com.bangkit.capstone.insightapp.R
 import com.bangkit.capstone.insightapp.databinding.ActivityFindOutBinding
 import com.bangkit.capstone.insightapp.databinding.ActivityLoginBinding
@@ -16,6 +19,10 @@ class FindOutActivity : AppCompatActivity() {
     private var _binding: ActivityFindOutBinding? = null
     private val binding get() = _binding!!
 
+    private var pengeluaranValid = false
+    private var umurValid = false
+    private var pendapatanValid = false
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,16 +30,53 @@ class FindOutActivity : AppCompatActivity() {
         _binding = ActivityFindOutBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        validateButton()
+        binding.umur.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                validateUmur()
+            }
+        })
+
+        binding.pendapatan.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                validatePendapatan()
+            }
+        })
+
+        binding.pengeluaran.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                validatePengeluaran()
+            }
+        })
+
         binding.findout.setOnClickListener {
 
-            val v1 : Float = binding.umur.text.toString().toFloat()
-            val v2 : Float = binding.pendapatan.text.toString().toFloat()
-            val v3 : Float = binding.pengeluaran.text.toString().toFloat()
+            val v1 : Int = binding.umur.text.toString().toInt()
+            val v2 : Int = binding.pendapatan.text.toString().toInt()
+            val v3 : Int = binding.pengeluaran.text.toString().toInt()
 
             val byteBuffer : ByteBuffer = ByteBuffer.allocateDirect(3 * 4)
-            byteBuffer.putFloat(v1)
-            byteBuffer.putFloat(v2)
-            byteBuffer.putFloat(v3)
+            byteBuffer.putInt(v1)
+            byteBuffer.putInt(v2)
+            byteBuffer.putInt(v3)
 
             val model = Mall.newInstance(this)
 
@@ -42,12 +86,12 @@ class FindOutActivity : AppCompatActivity() {
 
 // Runs model inference and gets result.
             val outputs = model.process(inputFeature0)
-            val outputFeature0 = outputs.outputFeature0AsTensorBuffer.floatArray
+            val outputFeature0 = outputs.outputFeature0AsTensorBuffer.intArray
 
             binding.hasil.text = """
-            hasil ${outputFeature0[0]}
-            hasil ${outputFeature0[1]}
-            hasil ${outputFeature0[2]}
+            Recommended ${outputFeature0[0]}
+            Not-Recommended ${outputFeature0[1]}
+            Very Recommended ${outputFeature0[2]}
         """.trimIndent()
 
 // Releases model resources if no longer used.
@@ -56,4 +100,63 @@ class FindOutActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun validatePengeluaran() {
+        val input = binding.pengeluaran.text.toString()
+        if (input.isEmpty()) {
+            pengeluaranValid = false
+            showPengeluaranAlert(true)
+        } else {
+            pengeluaranValid = true
+            showPengeluaranAlert(false)
+        }
+        validateButton()
+    }
+
+    private fun validatePendapatan() {
+        val input = binding.pendapatan.text.toString()
+        if (input.isEmpty()) {
+            pendapatanValid = false
+            showPendapatanAlert(true)
+        } else {
+            pendapatanValid = true
+            showPendapatanAlert(false)
+        }
+        validateButton()
+    }
+
+    private fun validateUmur() {
+        val input = binding.umur.text.toString()
+        if (input.isEmpty()) {
+            umurValid = false
+            showUmurAlert(true)
+        } else {
+            umurValid = true
+            showUmurAlert(false)
+        }
+        validateButton()
+    }
+
+    private fun validateButton() {
+        if (pengeluaranValid && pendapatanValid && umurValid) {
+            binding.findout.isEnabled = true
+            binding.findout.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_purple))
+        } else {
+            binding.findout.isEnabled = false
+            binding.findout.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray))
+        }
+    }
+
+    private fun showUmurAlert(isNotValid: Boolean) {
+        binding.umur.error = if (isNotValid) getString(R.string.not_valid) else null
+    }
+
+    private fun showPendapatanAlert(isNotValid: Boolean) {
+        binding.pendapatan.error = if (isNotValid) getString(R.string.not_valid) else null
+    }
+
+    private fun showPengeluaranAlert(isNotValid: Boolean) {
+        binding.pengeluaran.error = if (isNotValid) getString(R.string.not_valid) else null
+    }
+
 }
