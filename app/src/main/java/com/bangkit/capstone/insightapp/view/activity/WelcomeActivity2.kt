2297c37c43f0d34324f4dce142cb506f96c175c4
DESKTOP-5VPIR8G\Dicky arya pratama <dickyarya.personal@gmail.com>
2021-06-05 18:47:09 +0800

    add layout user registration about fashion

---
+++ app/src/main/java/com/bangkit/capstone/insightapp/view/activity/WelcomeActivity2.kt
@@ -0,0 +1,12 @@
+package com.bangkit.capstone.insightapp.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.capstone.insightapp.R
+
+class WelcomeActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome2)
+    }
+}
