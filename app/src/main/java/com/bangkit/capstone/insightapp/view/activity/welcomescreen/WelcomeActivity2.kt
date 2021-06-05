4a99e194881c4eabf3ae3ba20d86508795b1b151
DESKTOP-5VPIR8G\Dicky arya pratama <dickyarya.personal@gmail.com>
2021-06-05 21:54:08 +0800

    add textvalidation for pants and shoes, prepare for next step

--- app/src/main/java/com/bangkit/capstone/insightapp/view/activity/welcomescreen/WelcomeActivity2.kt
+++ app/src/main/java/com/bangkit/capstone/insightapp/view/activity/welcomescreen/WelcomeActivity2.kt
@@ -3,6 +3,9 @@ package com.bangkit.capstone.insightapp.view.activity.welcomescreen
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.core.content.ContextCompat
 import com.bangkit.capstone.insightapp.R
 import com.bangkit.capstone.insightapp.databinding.ActivityWelcome2Binding
 import com.bangkit.capstone.insightapp.databinding.ActivityWelcomeBinding
@@ -13,8 +16,8 @@ class WelcomeActivity2 : AppCompatActivity() {
     private var _binding: ActivityWelcome2Binding? = null
     private val binding get() = _binding!!
 
-    private var jenisShirtValid = false
-    private var namaShirtValid = false
+    private var jenisCelanaValid = false
+    private var namaCelanaValid = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +26,79 @@ class WelcomeActivity2 : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.hide()
 
+        binding.jenisStyleCelana.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                validateStyleCelana()
+            }
+
+        })
+
+        binding.namaCelana.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                validateNamaCelana()
+            }
+        })
+
+
+
         binding.next2.setOnClickListener {
             val intent = Intent(this, WelcomeActivity3::class.java)
             startActivity(intent)
         }
 
+    }
+
+    private fun validateNamaCelana() {
+        val input = binding.namaCelana.text.toString()
+        if (input.isEmpty()) {
+            namaCelanaValid = false
+            showNamaCelanaAlert(true)
+        } else {
+            namaCelanaValid = true
+            showNamaCelanaAlert(false)
+        }
+        validateButton()
+    }
+
+    private fun validateStyleCelana() {
+        val input = binding.jenisStyleCelana.text.toString()
+        if (input.isEmpty()) {
+            jenisCelanaValid = false
+            showJenisCelanaAlert(true)
+        } else {
+            jenisCelanaValid = true
+            showJenisCelanaAlert(false)
+        }
+        validateButton()
+    }
+
+    private fun showJenisCelanaAlert(isNotValid: Boolean) {
+        binding.jenisStyleCelana.error = if (isNotValid) getString(R.string.not_valid_empty) else null
+    }
+
+    private fun showNamaCelanaAlert(isNotValid: Boolean) {
+        binding.namaCelana.error = if (isNotValid) getString(R.string.not_valid_empty) else null
+    }
+
+    private fun validateButton() {
+        if (namaCelanaValid && jenisCelanaValid) {
+            binding.next2.isEnabled = true
+            binding.next2.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_blue_light))
+        } else {
+            binding.next2.isEnabled = false
+            binding.next2.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray))
+        }
     }
 }
--- app/src/main/java/com/bangkit/capstone/insightapp/view/activity/welcomescreen/WelcomeActivity3.kt
+++ app/src/main/java/com/bangkit/capstone/insightapp/view/activity/welcomescreen/WelcomeActivity3.kt
@@ -3,6 +3,9 @@ package com.bangkit.capstone.insightapp.view.activity.welcomescreen
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.core.content.ContextCompat
 import com.bangkit.capstone.insightapp.R
 import com.bangkit.capstone.insightapp.databinding.ActivityWelcome2Binding
 import com.bangkit.capstone.insightapp.databinding.ActivityWelcome3Binding
@@ -13,6 +16,9 @@ class WelcomeActivity3 : AppCompatActivity() {
     private var _binding: ActivityWelcome3Binding? = null
     private val binding get() = _binding!!
 
+    private var jenisSepatuValid = false
+    private var namaSepatuValid = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,9 +26,78 @@ class WelcomeActivity3 : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.hide()
 
+        binding.namaSepatu.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                validateNamaSepatu()
+            }
+
+        })
+
+        binding.jenisStyleSepatu.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                validateStyleSepatu()
+            }
+
+        })
+
         binding.next3.setOnClickListener {
             val intent = Intent(this, WelcomeActivity4::class.java)
             startActivity(intent)
         }
     }
+
+    private fun validateStyleSepatu() {
+        val input = binding.jenisStyleSepatu.text.toString()
+        if (input.isEmpty()) {
+            namaSepatuValid = false
+            showStyleSepatuAlert(true)
+        } else {
+            namaSepatuValid = true
+            showStyleSepatuAlert(false)
+        }
+        validateButton()
+    }
+
+    private fun validateNamaSepatu() {
+        val input = binding.namaSepatu.text.toString()
+        if (input.isEmpty()) {
+            jenisSepatuValid = false
+            showJenisSepatuAlert(true)
+        } else {
+            jenisSepatuValid = true
+            showJenisSepatuAlert(false)
+        }
+        validateButton()
+    }
+
+    private fun showJenisSepatuAlert(isNotValid: Boolean) {
+        binding.jenisStyleSepatu.error = if (isNotValid) getString(R.string.not_valid_empty) else null
+    }
+
+    private fun showStyleSepatuAlert(isNotValid: Boolean) {
+        binding.namaSepatu.error = if (isNotValid) getString(R.string.not_valid_empty) else null
+    }
+
+    private fun validateButton() {
+        if (namaSepatuValid && jenisSepatuValid) {
+            binding.next3.isEnabled = true
+            binding.next3.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_blue_light))
+        } else {
+            binding.next3.isEnabled = false
+            binding.next3.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray))
+        }
+    }
+
 }
